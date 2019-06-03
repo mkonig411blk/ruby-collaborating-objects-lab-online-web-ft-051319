30088c16a7f01f5327c7385c03e3870a1710c358
Margot Konig <margot.konig@blackrock.com>
2019-06-03 03:01:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,6 +13,7 @@ class Song
     artist = Artist.find_or_create_by_name(file_array[0])
     #use info from parsed filename to find artist
     new_song.artist = artist
+    artist.songs << new_song
     artist.save
     new_song
     #associate the newly created song with the found or created artist
